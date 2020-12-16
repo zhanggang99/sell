d4bf8bf6f365842a3a06a57c1101ba974558f8d7
zhanggangb <zhanggangb@yonyou.com>
2020-12-16 08:47:45 +0800

    init

---
+++ src/main/java/com/zg/sell/SellApplication.java
@@ -0,0 +1,15 @@
+package com.zg.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellApplication.class, args);
+        System.out.println("zhanggang");
+        System.out.println("testgit");
+    }
+
+}
