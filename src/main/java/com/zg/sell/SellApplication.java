ea8d2c9aa4e0e369eb36e39a11f30ae91d882e3c
zhanggangb <zhanggangb@yonyou.com>
2020-12-16 09:01:09 +0800

    init test5

--- src/main/java/com/zg/sell/SellApplication.java
+++ src/main/java/com/zg/sell/SellApplication.java
@@ -13,6 +13,7 @@ public class SellApplication {
         System.err.println("testgit2");
         System.out.println("test git3");
         System.out.println("test git 4K");
+        System.out.println("testg git 5K");
     }
 
 }
