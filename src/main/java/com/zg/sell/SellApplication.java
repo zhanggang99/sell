9d7e21a2c1a98e654f3bec692fbfff771c1af539
zhanggangb <zhanggangb@yonyou.com>
2020-12-16 09:02:21 +0800

    delte

--- src/main/java/com/zg/sell/SellApplication.java
+++ src/main/java/com/zg/sell/SellApplication.java
@@ -8,11 +8,6 @@ public class SellApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
-        System.out.println("zhanggang");
-        System.out.println("testgit");
-        System.err.println("testgit2");
-        System.out.println("test git3");
-        System.out.println("test git 4K");
         System.out.println("testg git 5K");
     }
 
