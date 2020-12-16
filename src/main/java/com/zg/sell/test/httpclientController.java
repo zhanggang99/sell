0875d6298f21b4efc5d9b92874f5a63b6b212cb5
zhanggangb <zhanggangb@yonyou.com>
2020-12-16 09:20:46 +0800

    sql add

---
+++ src/main/java/com/zg/sell/test/httpclientController.java
@@ -0,0 +1,13 @@
+package com.zg.sell.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class httpclientController {
+
+    @GetMapping("dohttpclienttest")
+    public String doGetClientTest(){
+        return "dohttpclienttest";
+    }
+}
---
+++ src/test/java/com/zg/sell/test/httpclientControllerTest.java
@@ -0,0 +1,16 @@
+package com.zg.sell.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class httpclientControllerTest {
+
+    //https://blog.csdn.net/justry_deng/article/details/81042379
+    @Test
+    public void dohttpclienttest(){
+
+    }
+}
